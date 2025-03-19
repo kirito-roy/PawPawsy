cb3d5c8d82847adb3cc26af551bc0770603f7bbe
sumit <roy184433@gmail.com>
2025-03-19 16:21:55 +0530

    fix file system

--- src/app/service/dto/student.ts
+++
@@ -1,7 +0,0 @@
-export interface StudentDTO {
-
-  name: string;
-  roll: string;
-  phoneNumber: string;
-  className: "c1" | "c2"| "c3" | "c4" | "c5" | "c6" | "c7" | "c8" | "c9" | "c10" | "c11" | "c12";
-}
