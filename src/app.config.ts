c0fdfbd4a3b65a68e51d8ea6acf525bc17848430
sumit <roy184433@gmail.com>
2025-04-09 18:17:34 +0530

    login with google

--- src/app.config.ts
+++ src/app.config.ts
@@ -27,7 +27,15 @@ export const appConfig: ApplicationConfig = {
       }),
       withEnabledBlockingInitialNavigation()
     ),
-    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideFirebaseApp(() => initializeApp({
+      apiKey: "AIzaSyDlqLc2Fq-tp_LIy1h5k0VXnl_wiBnUKY0",
+      authDomain: "pawpawsy.firebaseapp.com",
+      projectId: "pawpawsy",
+      storageBucket: "pawpawsy.firebasestorage.app",
+      messagingSenderId: "153275426304",
+      appId: "1:153275426304:web:d423fd9e8e211b28b71ae5",
+      measurementId: "G-P807ZK4TF7"
+    })),
     provideAuth(() => getAuth()),
     {
       provide: HTTP_INTERCEPTORS,
