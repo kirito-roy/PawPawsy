9803fa098161423ba9878396db6d83c493cf877b
sumit <roy184433@gmail.com>
2025-05-03 17:40:21 +0530

    add a user profile page

--- src/app/components/app-base/user-profile/user-profile.component.ts
+++ src/app/components/app-base/user-profile/user-profile.component.ts
@@ -1,21 +1,34 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { ImageModule } from 'primeng/image';
+import { InputText } from 'primeng/inputtext';
+import {AuthService} from "src/app/components/core/services/auth.service";
 
 @Component({
   selector: 'app-user-profile',
-  imports: [CommonModule],
+  imports: [CommonModule,FormsModule,InputText,ButtonModule,ImageModule],
   templateUrl: './user-profile.component.html',
-  styleUrl: './user-profile.component.scss',
+  // styleUrl: './user-profile.component.scss',
 })
 export class UserProfileComponent implements OnInit {
   imgSrc!:string;
   logedIN:boolean=false;
-  constructor(){
+  userName:string='User';
+  email:string='user@gmail.com';
+  phone:string='1234567890';
+  editFirstPanel:boolean=false;
+  constructor(private auth :AuthService){
 
   }
   ngOnInit(): void {
     this.imgSrc='assets/img/feather1.jpg';
-    this.logedIN=true;
+    this.isloggedIn();
+  }
+  isloggedIn(){
+    const login = this.auth.hasToken();
+    this.logedIN = login;
   }
 
 }
